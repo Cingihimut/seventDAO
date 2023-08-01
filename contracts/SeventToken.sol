0292334d2cab3066498d1abce6cfe1232b58a432
cingih imut <singgihbrilian.tara06@gmail.com>
2023-08-01 19:15:39 +0700

    add contracts and another env

---
+++ contracts/SeventToken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract SeventToken {
+    string public constant name = "SeventToken";
+    string public constant symbol = "St";
+    uint256 public constant initialSupply = 200000000 * 10**18;
+    uint8 public constant decimal = 18;
+
+    constructor() {
+        //
+    }
+
+}
