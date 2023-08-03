224f394a6b3f9fc3ea33f6b1c7697042c9dae23a
cingih imut <singgihbrilian.tara06@gmail.com>
2023-08-03 22:37:43 +0700

    setup second token

---
+++ contracts/Go.sol
@@ -0,0 +1,11 @@
+// contracts/Go.sol
+// SPDX-License-Identifier: MIT
+import "../../../MainContract/contracts/SeventFinance.sol";
+// import "./Voting.sol";
+
+pragma solidity ^0.8.19;
+
+contract Go is SeventFinance {
+    //address public Voting
+
+}
