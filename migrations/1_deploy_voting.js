14f8a7bba8b60a7c1fcbc599cd2196fbd666ac28
cingih imut <singgihbrilian.tara06@gmail.com>
2023-08-06 00:04:39 +0700

    update script migrations

--- migrations/1_deploy_voting.js
+++ migrations/1_deploy_voting.js
@@ -1,5 +1,6 @@
 const Voting = artifacts.require("Voting");
 
 module.exports = function (deployer) {
-    deployer.deploy(Voting);
+    const votingTokenAddress = "0x0fd466C1b04Ad37502f1eE31392E398B2C01f29E";
+    deployer.deploy(Voting, votingTokenAddress);
 }
